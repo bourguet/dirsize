8b5f27e716e84731a34216d2d838f6543616a4fa
Jean-Marc Bourguet <jm@bourguet.org>
2012-09-20 19:43:31 +0200

    Fixed issue 4, use block size. Added copyright notice.

--- src/dirsize.cpp
+++ src/dirsize.cpp
@@ -1,4 +1,39 @@
 // -*- C++ -*-
+//
+// dirsize
+// Copyright (C) 2012  Jean-Marc Bourguet
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//   * Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//   * Neither the name of Jean-Marc Bourguet nor the names of other
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Show the directory usage
+//
 // ----------------------------------------------------------------------------
 
 #include <algorithm>
@@ -17,6 +52,9 @@
 #include <vector>
 #include <iomanip>
 #include <iterator>
+
+// stat returns info in number of blocks of 512 bytes
+long long const blockSize = 512;
 
 // ----------------------------------------------------------------------------
 
@@ -50,7 +88,7 @@ public:
 
 std::ostream& operator<<(std::ostream& os, Info const& i)
 {
-    os << std::setw(15) << i.size << " " << i.name;
+    os << std::setw(15) << i.size*blockSize << " " << i.name;
 } // operator<<
 
 // ----------------------------------------------------------------------------
@@ -74,7 +112,7 @@ private:
 
 bool IsSmallerThan::operator()(Info const& i) const
 {
-    return i.size < myLimit;
+    return i.size*blockSize < myLimit;
 } // IsSmallerThan::operator()
 
 // ----------------------------------------------------------------------------
@@ -117,7 +155,7 @@ private:
 
 bool IsHierSizeSmallerThan::operator()(HierInfo const& i) const
 {
-    return i.size < myLimit;
+    return i.size*blockSize < myLimit;
 } // IsSmallerThan::operator()
 
 // ----------------------------------------------------------------------------
@@ -138,7 +176,7 @@ std::vector<Info> theFlatInfo;
 
 void HierInfo::show(int indent, std::vector<bool> need = std::vector<bool>())
 {
-    std::cout << std::setw(15) << size << " ";
+    std::cout << std::setw(15) << size*blockSize << " ";
     for (int i = 0; i+1 < indent; ++i)
         if (need[i])
             std::cout << "| ";
@@ -275,6 +313,11 @@ HierInfo readDirectoryStructure(std::string const& dir)
                         HierInfo subInfo = readDirectoryStructure(path);
                         subInfo.name = name;
                         subDirSize += subInfo.size;
+                        result.subInfo.push_back(subInfo);
+                    } else {
+                        HierInfo subInfo;
+                        subInfo.name = name;
+                        subInfo.size = info.st_blocks;
                         result.subInfo.push_back(subInfo);
                     }
                 }
